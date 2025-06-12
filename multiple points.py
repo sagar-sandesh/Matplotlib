a2d07802a84813da19bf888de0fee72286dc9baf
Sagar Sandesh Oli <sagaroli450@gmail.com>
2025-06-13 00:10:24 +0300

    face color

---
+++ multiple points.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1,7,2,8])
+ypoints = np.array([ 4,13,28,15])
+
+plt.plot(xpoints, ypoints)
+plt.show()
---
+++ single point plots.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10, 5, 7])
+
+# plt.plot(ypoints, marker='o')
+# plt.plot(ypoints, marker='x')
+
+# plt.plot(ypoints, 'o:r')
+
+# plt.plot(ypoints, marker ='o', ms = 20)
+
+plt.plot(ypoints, marker = 'o', ms = 20, mfc = 'r')
+
+plt.show()
