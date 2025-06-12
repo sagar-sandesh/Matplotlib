c921e2a6dc1619c6bfa82fb14d9e36bfd0c179ac
Sagar Sandesh Oli <sagaroli450@gmail.com>
2025-06-13 00:19:40 +0300

    Multiple lines

--- matplotlib line.py
+++ matplotlib line.py
@@ -1,11 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-ypoints = np.array([3, 8, 1, 10])
+# ypoints = np.array([3, 8, 1, 10])
+#
+#
+# # plt.plot(ypoints, linestyle = 'dotted')
+#
+# # plt.plot(ypoints, linestyle = 'dashed')
+#
+# plt.plot(ypoints, color = 'r')
 
-# plt.plot(ypoints, linestyle = 'dotted')
+y1 = np.array([3, 8, 1, 10])
+y2 = np.array([6, 2, 7, 11])
 
-# plt.plot(ypoints, linestyle = 'dashed')
+plt.plot(y1)
+plt.plot(y2)
 
-plt.plot(ypoints, color = 'r')
 plt.show()
