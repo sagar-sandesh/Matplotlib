2a455bdbc18cb70cbf80e44b01a44716c783eb5f
Sagar Sandesh Oli <sagaroli450@gmail.com>
2025-06-12 23:53:15 +0300

    ploting points in dots

--- pyplot.py
+++ pyplot.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([2,10])
+xpoints = np.array([0,10])
 ypoints = np.array([0,250])
+zpoints = np.array([0,220])
 
-plt.plot(xpoints, ypoints)
+plt.plot(xpoints, ypoints, zpoints)
 plt.show()
+
+# Plotting without the lines
+# xpoints = np.array([0,10])
+# ypoints = np.array([0,20])
+#
+# plt.plot(xpoints, ypoints, 'o')
+# plt.show()
